725f1bee3afa562091d2c5bd1619dd9320dc204f
Mateus da Silva Sousa <mateus.wtv@gmail.com>
2021-06-12 13:27:35 -0300

    add SearchSelect

---
+++ src/components/filter-control/SearchSelect.vue
@@ -0,0 +1,34 @@
+<template>
+  <b-form-group
+    :id="`select-${label}`"
+    :label="label"
+    :label-for="`select-${label}`"
+    label-size="sm"
+    label-align="left"
+  >
+    <b-form-select
+      value="value"
+      @submit="updateSelect($event)"
+      :id="`select-${label}`"
+      :options="options"
+    ></b-form-select>
+  </b-form-group>
+</template>
+
+<script lang="ts">
+import { Component, Emit, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class extends Vue {
+  @Prop() value!: string;
+  @Prop() label!: string;
+  @Prop() options!: string[];
+
+  @Emit("input")
+  updateSelect(e: string): string {
+    return e;
+  }
+}
+</script>
+
+<style scoped></style>
