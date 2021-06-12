526ae8550131be3c8d23b7c89a07ba13f9b34368
Mateus da Silva Sousa <mateus.wtv@gmail.com>
2021-06-12 13:27:13 -0300

    add SearchInput

---
+++ src/components/filter-control/SearchInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <b-form-group
+    :id="`field-${label}`"
+    :label="label"
+    :label-for="`search-label-${label}`"
+    label-size="sm"
+    label-align="left"
+  >
+    <b-form-input
+      :id="`search-label-${label}`"
+      :value="value"
+      @input="updateValue($event)"
+    ></b-form-input>
+  </b-form-group>
+</template>
+
+<script lang="ts">
+import { Component, Emit, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class extends Vue {
+  @Prop() private value!: string;
+  @Prop() private label!: string;
+
+  @Emit("input")
+  updateValue(e: string): string {
+    return e;
+  }
+}
+</script>
+
+<style scoped></style>
