f990f38e5f933bb1c60c8fcc718b6cdaeacbe99f
Mateus da Silva Sousa <mateus.wtv@gmail.com>
2021-06-12 13:28:17 -0300

    add SearchFilter to Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <search-filter />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
+import SearchFilter from "@/components/SearchFilter.vue";
 @Component({
   components: {
-    HelloWorld,
+    SearchFilter,
   },
 })
 export default class Home extends Vue {}
