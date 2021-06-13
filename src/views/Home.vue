5da688a61692e4792e141b46cec4ae7e36298168
Mateus da Silva Sousa <mateus.wtv@gmail.com>
2021-06-13 16:10:19 -0300

    fetch drones

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,10 +7,35 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import SearchFilter from "@/components/SearchFilter.vue";
+import { getDrones } from "@/services/getDrones";
+import { drone } from "@/types/dronesResponse";
 @Component({
   components: {
     SearchFilter,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  drones: drone[] | [] = [];
+
+  async fetchDrones(page: string): Promise<void> {
+    try {
+      const response = await getDrones(page);
+      if (response) this.drones = response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  mounted(): void {
+    this.$router.push({ query: { limit: "20" } }).catch((error) => {
+      if (error.name != "NavigationDuplicated") {
+        throw error;
+      }
+    });
+    let page = this.$route.query.page;
+    if (!page) page = "1";
+    if (typeof page === "string") {
+      this.fetchDrones(page);
+    }
+  }
+}
 </script>
