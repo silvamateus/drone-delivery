ab94ebfa87e4813d5f8715189548bfe4faacee10
Mateus da Silva Sousa <mateus.wtv@gmail.com>
2021-06-13 16:08:21 -0300

    create getDrones service

---
+++ src/services/getDrones.ts
@@ -0,0 +1,24 @@
+import { AxiosResponse } from "axios";
+import instance from "./instance";
+
+export const getDrones = async (
+  page: string
+): Promise<AxiosResponse<any> | undefined> => {
+  try {
+    const response = instance.get(`?_page=${page}&_limit=20`);
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const filterDrones = async (
+  filter: string
+): Promise<AxiosResponse<any> | undefined> => {
+  try {
+    const response = instance.get(`?${filter}`);
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
