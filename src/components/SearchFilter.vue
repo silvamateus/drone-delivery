e704225a751abcf91305d17ad433ddfb7fb7c8f5
Mateus da Silva Sousa <mateus.wtv@gmail.com>
2021-06-13 16:09:40 -0300

    fix fields size in medium screen sizes

--- src/components/SearchFilter.vue
+++ src/components/SearchFilter.vue
@@ -1,17 +1,18 @@
 <template>
   <b-container>
     <b-row>
-      <b-col lg="2">
+      <b-col md="2" lg="2">
         <search-input label="Drone ID" v-model="droneID"></search-input
       ></b-col>
-      <b-col lg="4">
+      <b-col md="4" lg="4">
         <search-input label="Name" v-model="droneName"></search-input
       ></b-col>
-      <b-col lg="2">
+      <b-col md="2" lg="2">
         <search-select label="Current Fly" v-model="droneFly"></search-select>
       </b-col>
-      <search-select label="Status" v-model="droneStatus"></search-select>
-      <b-col lg="2"></b-col>
+      <b-col md="2" lg="2">
+        <search-select label="Status" v-model="droneStatus"></search-select>
+      </b-col>
     </b-row>
   </b-container>
 </template>
