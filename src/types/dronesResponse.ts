2ae82b350fb0247b9f93b983c9b8c1e8e97c43de
Mateus da Silva Sousa <mateus.wtv@gmail.com>
2021-06-13 16:08:51 -0300

    create drone interface

---
+++ src/types/dronesResponse.ts
@@ -0,0 +1,11 @@
+export interface drone {
+  address: string;
+  avarage_speed: number;
+  battery: number;
+  fly: number;
+  id: number;
+  image: string;
+  max_speed: number;
+  name: string;
+  status: string;
+}
