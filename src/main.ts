1b74cde0941a6781032c895b369e243d1efcc0e6
Mateus da Silva Sousa <mateus.wtv@gmail.com>
2021-06-14 19:14:00 -0300

    add ress

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import "ress";
 
 Vue.use(BootstrapVue);
 Vue.use(BootstrapVueIcons);
